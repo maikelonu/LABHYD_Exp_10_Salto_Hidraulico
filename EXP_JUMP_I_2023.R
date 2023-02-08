faeb98fa84e996f03f9554be73ac4a03d4fe9450
Maikel Mendez <maikel_m@yahoo.com>
2023-02-08 14:20:14 -0600

    Update and rename EXP_JUMP_I_2019.R to EXP_JUMP_I_2023.R

--- EXP_JUMP_I_2023.R
+++ EXP_JUMP_I_2023.R
@@ -12,7 +12,6 @@
 # https://www.scopus.com/authid/detail.uri?authorId=51665581300
 # https://scholar.google.com/citations?user=JnmSVFYAAAAJ&hl=en
 # https://www.youtube.com/c/maikelmendez
-# https://twitter.com/MaikelMendezM
 # https://github.com/maikelonu
 # //////////////////////////////////////////////////////////////////////////////////
 
